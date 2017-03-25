d2e9974e4f97b8c1d038de65fb294065f6449187
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2017-03-25 12:56:29 -0400

    Initial commit: python server

---
+++ obamo.py
@@ -0,0 +1,62 @@
+import math
+import os
+
+from functools import lru_cache
+
+import readtime
+import requests
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+
+app = Flask(__name__)
+
+
+FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
+NEURO_API_KEY = os.environ['NEURO_API_KEY']
+AUTH_HEADER = {'Authorization': f'Client-Key {NEURO_API_KEY}'}
+
+
+@lru_cache(maxsize=1024)
+def get_readtime_from_url(url):
+    news_info = requests.get(url, headers=AUTH_HEADER).json()
+    summary_html = news_info['summary']
+    body_html = news_info['body']['html']
+    summary_time = readtime.of_html(summary_html)
+    body_time = readtime.of_html(body_html)
+    total_seconds = summary_time.seconds + body_time.seconds
+    readtime_json = {
+        "total": {
+            "seconds": total_seconds,
+            "minutes": math.ceil(total_seconds/60),
+        },
+        "sections": {
+            "summary" : {
+                "seconds": summary_time.seconds,
+                "minutes": summary_time.minutes,
+            },
+            "body": {
+                "seconds": body_time.seconds,
+                "minutes": body_time.minutes,
+            },
+        },
+    }
+    return readtime_json
+
+
+@app.route('/ping')
+def ping():
+    return 'pong'
+
+
+@app.route('/readtime', methods=['POST'])
+def get_readtime():
+    url = request.json['url']
+    result = get_readtime_from_url(url)
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=FLASK_DEBUG, host='0.0.0.0', port=5000)
