e5eb4f70a5766e3cfb6d7a36da2afe101cb92206
Samuel Dion-Girardeau <samuel.diongirardeau@gmail.com>
2017-03-26 00:01:31 -0400

    Stop breaking on empty input

--- obamo.py
+++ obamo.py
@@ -19,13 +19,19 @@ NEURO_API_KEY = os.environ['NEURO_API_KEY']
 AUTH_HEADER = {'Authorization': f'Client-Key {NEURO_API_KEY}'}
 
 
+class EmptyResult:
+
+    seconds = 0
+    minutes = 0
+
+
 @lru_cache(maxsize=1024)
 def get_readtime_from_url(url):
     news_info = requests.get(url, headers=AUTH_HEADER).json()
     summary_html = news_info['summary']
     body_html = news_info['body']['html']
-    summary_time = readtime.of_html(summary_html)
-    body_time = readtime.of_html(body_html)
+    summary_time = readtime.of_html(summary_html) if summary_html else EmptyResult
+    body_time = readtime.of_html(body_html) if body_html else EmptyResult
     total_seconds = summary_time.seconds + body_time.seconds
     readtime_json = {
         "total": {
